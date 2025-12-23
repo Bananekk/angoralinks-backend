9dd4c075ca4ef70021ba90e8ffc0a6f7a14cbe28
iobywatel <mnich.mniszkowy24@gmail.com>
2025-12-24 00:08:24 +0100

    Zmiana domeny

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors({
     origin: [
         'http://localhost:5173',
         'http://localhost:3000',
-        'https://angoralinks.vercel.app'
+        'https://angoralinks.pl'
     ],
     credentials: true
 }));
